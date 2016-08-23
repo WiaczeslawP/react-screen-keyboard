e37ec843c289d8c2d5d7b7d76fd4b9bc1264fa33
Пучков Вячеслав <puckovvaceslav@biriukov.local>
2016-08-23 17:19:35 +0300

    fix package

--- Keyboard.js
+++ Keyboard.js
@@ -1,5 +1,5 @@
 var Keyboard = require('./lib/Keyboard');
-var Keyboard = require('./lib/KeyboardButton');
+var KeyboardButton = require('./lib/KeyboardButton');
 
 module.exports = Keyboard.default || Keyboard;
 module.exports.KeyboardButton = KeyboardButton.default || KeyboardButton;
