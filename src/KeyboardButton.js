86ba30144c13fe93dce7f37d53a6ff90dabba15c
Viacheslav Puchkov <puchkov@tochkak.ru>
2017-02-24 16:57:19 +0300

    add eslint config

--- src/KeyboardButton.js
+++ src/KeyboardButton.js
@@ -27,7 +27,7 @@ export default class KeyboardButton extends PureComponent {
 		return (
 			<button
 				type="button"
-				className={'keyboard-button' + ' ' + this.props.classes}
+				className={`${'keyboard-button' + ' '}${this.props.classes}`}
 				onClick={this.props.isDisabled ? null : this.handleClick}
 				autoFocus={this.props.autofocus}
 				disabled={this.props.isDisabled}
--- src/layouts/LatinLayout.js
+++ src/layouts/LatinLayout.js
@@ -2,7 +2,7 @@ export default [
 	['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
 	['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
 	['z', 'x', 'c', 'v', 'b', 'n', 'm']
-]
+];
 
 
 // export default {
