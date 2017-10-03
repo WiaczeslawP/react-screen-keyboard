4f72d9ea98e6a04b4fbe8ee4aea5dba47bef9663
Goran Martinjak <goran@netgen.hr>
2017-10-03 10:47:00 +0200

    add only numbers layout

--- Keyboard.js
+++ Keyboard.js
@@ -2,8 +2,10 @@ var Keyboard = require('./lib/Keyboard');
 var KeyboardButton = require('./lib/KeyboardButton');
 var LatinLayout = require('./lib/layouts/LatinLayout');
 var CyrillicLayout = require('./lib/layouts/CyrillicLayout');
+var NumericLayout = require('./lib/layouts/NumericLayout');
 
 module.exports = Keyboard.default || Keyboard;
 module.exports.KeyboardButton = KeyboardButton.default || KeyboardButton;
 module.exports.LatinLayout =  LatinLayout.default || LatinLayout;
 module.exports.CyrillicLayout =  CyrillicLayout.default || CyrillicLayout;
+module.exports.NumericLayout =  NumericLayout.default || NumericLayout;
